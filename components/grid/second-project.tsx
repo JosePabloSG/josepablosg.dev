3d835f2c57b204d8e33d06227a0b3e3780af74aa
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2025-01-24 17:04:53 -0600

    fix: update project image filename for consistency

--- components/grid/second-project.tsx
+++ components/grid/second-project.tsx
@@ -1,4 +1,4 @@
-import projectImage from '@/public/projects/hhrr.png';
+import projectImage from '@/public/projects/human-resources-management.png';
 import Project from '../project';
 
 export default function SecondProject() {
