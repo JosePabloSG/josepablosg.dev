ab7a41b355c3cc10f0ba566b0695cdad3a998365
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2025-02-05 23:17:12 -0600

    feat: add footer with logo and copyright information for improved branding

--- app/page.tsx
+++ app/page.tsx
@@ -18,19 +18,7 @@ export default function Home() {
                     ))}
                 </GridLayout>
             </main>
-            <footer className="py-4">
-                <Container className="flex justify-center items-center">
-                    <div className="flex items-center space-x-2">
-                        <Image src="/logo/Logo.svg" alt="José Pablo Logo" width={24} height={24} />
-                        <p className="text-sm text-muted-foreground">
-                            © 2025 José Pablo - Inspired by{" "}
-                            <Link href="https://nevflynn.com/" className="underline hover:text-primary">
-                                NevFlynn
-                            </Link>
-                        </p>
-                    </div>
-                </Container>
-            </footer>
+    
         </>
     )
 }
