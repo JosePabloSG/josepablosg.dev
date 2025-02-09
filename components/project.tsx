6461e794427ea1b3cac2c54836e2a6f0146270f9
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2025-02-09 11:52:56 -0600

    feat: improve Project component styling and animations for better user experience

--- components/project.tsx
+++ components/project.tsx
@@ -19,21 +19,22 @@ export default function Project({
                 src={projectImage}
                 alt={toKebabCase(projectName)}
                 fill
-                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                className='object-cover'
-                placeholder='blur'
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className="object-cover"
+                placeholder="blur"
                 priority
             />
-            <div className='absolute bottom-3 left-3'>
+            <div className="absolute bottom-3 left-3">
                 <Anchor
-                    className='cancel-drag size-10 justify-end transition-all ease-in-out group-hover:w-full'
+                    className="cancel-drag size-10 justify-end transition-[width] duration-300 ease-in-out group-hover:w-full"
                     href={`/projects/${toKebabCase(projectName)}`}
-                    aria-label={projectName}>
-                    <span className='hidden whitespace-nowrap opacity-0 transition-all duration-300 ease-in group-hover:translate-x-0 group-hover:opacity-100 md:inline'>
+                    aria-label={projectName}
+                >
+                    <span className="hidden whitespace-nowrap opacity-0 transition-transform duration-300 ease-in-out group-hover:translate-x-0 group-hover:opacity-100 md:inline">
                         {projectName}
                     </span>
                     <span>
-                        <FaArrowRight className='-rotate-45 transition-transform duration-300 group-hover:rotate-0' />
+                        <FaArrowRight className="-rotate-45 transition-transform duration-300 ease-in-out group-hover:rotate-0" />
                     </span>
                 </Anchor>
             </div>
